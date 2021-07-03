5a2826cec319297627cd1a9f8b0680d4463d1d38
Keiku <kuroyanagi.keiichi@gmail.com>
2021-07-03 09:58:08 +0900

    add custom dataset

---
+++ datasets.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import re
+import numpy as np
+import pandas as pd
+import torch
+from PIL import Image
+
+
+class CIFAR10Dataset(torch.utils.data.Dataset):
+    def __init__(self, cfg, train, transform=None):
+        super(CIFAR10Dataset, self).__init__()
+        self.transform = transform
+        self.cfg = cfg
+        self.split_dir = 'train' if train else 'test'
+        self.root_dir = Path(cfg.dataset.root_dir)
+        self.image_dir = self.root_dir / 'cifar' / self.split_dir
+        self.file_list = [p.name for p in self.image_dir.rglob('*') if p.is_file()]
+        self.labels = [re.split('_|\.', l)[1] for l in self.file_list]
+        self.targets = self.label_mapping(cfg)
+
+    def label_mapping(self, cfg):
+        labels = self.labels
+        label_mapping_path = Path(cfg.dataset.root_dir) / 'cifar/labels.txt'
+        df_label_mapping = pd.read_table(label_mapping_path.as_posix(), names=['label'])
+        df_label_mapping['target'] = range(10)
+
+        label_mapping_dict = dict(zip(
+            df_label_mapping['label'].values.tolist(),
+            df_label_mapping['target'].values.tolist()))
+
+        targets = [label_mapping_dict[i] for i in labels]
+        return targets
+
+    def __getitem__(self, index):
+        cfg = self.cfg
+        image_dir = self.image_dir
+        file_list = self.file_list
+        targets = self.targets
+
+        filename = file_list[index]
+        targets = targets[index]
+        image_path = image_dir / filename
+        image = Image.open(image_path.as_posix())
+
+        if self.transform is not None:
+            transform = self.transform
+            image = transform(image)
+
+        return image, targets
+
+    def __len__(self):
+        return len(self.file_list)
