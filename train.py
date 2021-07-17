d29fd733e2794f4659a716fcb558dffe753645e5
Keiku <kuroyanagi.keiichi@gmail.com>
2021-07-17 22:29:38 +0900

    add resume training

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import hydra
 import torch
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
+from pathlib import Path
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
@@ -22,12 +23,17 @@ def main(cfg: DictConfig) -> None:
     os.environ["CUDA_VISIBLE_DEVICES"] = cfg.runs.gpu_id
 
     if cfg.runs.logger == "wandb":
-        logger = WandbLogger(name=cfg.model.classifier, project="cifar10")
+        logger = WandbLogger(name=cfg.model.classifier, project=cfg.model.project)
     elif cfg.runs.logger == "tensorboard":
-        logger = TensorBoardLogger(cfg.train.tensorboard_dir, name=cfg.model.classifier)
+        logger = TensorBoardLogger(cfg.train.tensorboard_dir, name=cfg.model.classifier, version=cfg.model.version)
 
     checkpoint = ModelCheckpoint(monitor="acc/val", mode="max", save_last=True)
     lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    if cfg.train.resume and Path(cfg.train.checkpoint).exists():
+        resume_checkpoint = cfg.train.checkpoint
+    else:
+        resume_checkpoint = None
 
     trainer = Trainer(
         fast_dev_run=cfg.runs.dev,
@@ -38,7 +44,7 @@ def main(cfg: DictConfig) -> None:
         log_every_n_steps=1,
         max_epochs=cfg.train.num_epochs,
         precision=cfg.runs.precision,
-        resume_from_checkpoint=cfg.train.checkpoint,
+        resume_from_checkpoint=resume_checkpoint,
         callbacks=[checkpoint, lr_monitor],
     )
 
