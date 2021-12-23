c902aea53232768e74f0f60c426fb1991e89cc40
Keiku <kuroyanagi.keiichi@gmail.com>
2021-12-23 16:36:04 +0900

    fix configure_optimizers problem

--- model.py
+++ model.py
@@ -6,6 +6,9 @@ import torchmetrics
 from models.resnet import resnet18, resnet34, resnet50
 from schduler import WarmupCosineLR
 
+from pytorch_lightning import LightningModule, Trainer, LightningDataModule
+
+
 classifiers = {
     "resnet18": resnet18(),
     "resnet34": resnet34(),
@@ -14,7 +17,7 @@ classifiers = {
 
 
 class LitCIFAR10Model(pl.LightningModule):
-    def __init__(self, cfg, *args, **kwargs):
+    def __init__(self, cfg, trainer, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.save_hyperparameters()
         self.cfg = cfg
@@ -23,6 +26,7 @@ class LitCIFAR10Model(pl.LightningModule):
         self.accuracy = torchmetrics.Accuracy()
 
         self.model = self.get_model(cfg)
+        self.trainer = trainer
 
     def get_model(self, cfg):
         if cfg.model.implementation == "scratch":
@@ -61,6 +65,12 @@ class LitCIFAR10Model(pl.LightningModule):
         loss, accuracy = self.forward(batch)
         self.log("acc/test", accuracy)
 
+    def setup_steps(self, stage=None):
+        # NOTE There is a problem that len(train_loader) does not work.
+        # After updating to 1.5.2, NotImplementedError: `train_dataloader` · Discussion #10652 · PyTorchLightning/pytorch-lightning https://github.com/PyTorchLightning/pytorch-lightning/discussions/10652
+        train_loader = self.trainer._data_connector._train_dataloader_source.dataloader()
+        return len(train_loader)
+
     def configure_optimizers(self):
         cfg = self.cfg
         optimizer = torch.optim.SGD(
@@ -70,7 +80,7 @@ class LitCIFAR10Model(pl.LightningModule):
             momentum=0.9,
             nesterov=True,
         )
-        total_steps = cfg.train.num_epochs * len(self.train_dataloader())
+        total_steps = cfg.train.num_epochs * self.setup_steps(self)
         scheduler = {
             "scheduler": WarmupCosineLR(
                 optimizer, warmup_epochs=total_steps * 0.3, max_epochs=total_steps
